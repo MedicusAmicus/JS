ff28438b3f4a00e84b077056b1e72641e6281f3f
Alex <medicusamicus@simferopol.in>
2022-05-19 14:42:40 +0300

    add 0.js

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,16 +1,1 @@
-console.log("Hello world");
-
-let item_1 = 5;
-console.log(item_1);
-
-let item_2 = 3;
-console.log(item_2);
-
-let item_3 = item_1 + item_2;
-console.log(item_3);
-
-let item_4 = "Yolochka";
-console.log(item_4);
-
-console.log(item_3 + item_4);
-console.log(item_4 * item_3);
+for (let n=1; n <= 10; n++) {console.log((2 ** n)+"\r\n");}
